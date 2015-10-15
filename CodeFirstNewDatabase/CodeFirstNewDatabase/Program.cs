9a6c38399918946cb413bbc40c238b6ca8b532a2
Jasko96 <jasmin.becarevic@gmail.com>
2015-10-15 20:45:21 +0200

    Init

---
+++ CodeFirstNewDatabase/CodeFirstNewDatabase/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+//https://msdn.microsoft.com/en-us/data/jj193542.aspx
+//Autor: Jasmin Becarevic - 5BHET - 15.10.2015
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                // Create and save a new Blog 
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database 
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+
+            }
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class Favourite_OnePiece_Character
+    {
+        [Key]
+       public int Id { get; set; } //Key muss definiert werden sonst funktioniert es nicht Id nicht von belangen
+
+       public bool Ruffy { get; set; }
+       public bool Zorro { get; set; }
+       public bool Lysop { get; set; }
+       public bool Sanji { get; set; }
+       public bool Nami { get; set; }
+       public bool Chopper { get; set; }
+       public bool Robin { get; set; }
+       public bool Franky { get; set; }
+       public bool Brook { get; set; }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Favourite_OnePiece_Character> Favourite_OnePiece_Character { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+        }
+    }
+}
